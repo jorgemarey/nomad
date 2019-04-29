e7314f281574f63558fa06c4b7d23e84b2ff7124
Jorge Marey <jorge.marey.oton.next@bbva.com>
2019-04-29 13:28:38 +0200

    Add improved autopilot

--- nomad/server_setup_oss.go
+++ nomad/server_setup_oss.go
@@ -5,13 +5,14 @@ package nomad
 import (
 	"github.com/hashicorp/consul/agent/consul/autopilot"
 	log "github.com/hashicorp/go-hclog"
+	improvedAutopilot "github.com/jorgemarey/autopilot"
 )
 
 type EnterpriseState struct{}
 
 func (s *Server) setupEnterprise(config *Config) error {
 	// Set up the OSS version of autopilot
-	apDelegate := &AutopilotDelegate{s}
+	apDelegate := improvedAutopilot.New(s.logger.StandardLogger(&log.StandardLoggerOptions{InferLevels: true}), &AutopilotDelegate{s})
 	s.autopilot = autopilot.NewAutopilot(s.logger.StandardLogger(&log.StandardLoggerOptions{InferLevels: true}), apDelegate, config.AutopilotInterval, config.ServerHealthInterval)
 
 	return nil
