f5a7bbd45df47bb0372e24a38d51b28a0a554bf6
Jorge Marey <jorge.marey.oton.next@bbva.com>
2025-02-12 18:42:36 +0100

    Change consul and vault fingerprints

--- client/fingerprint/vault.go
+++ client/fingerprint/vault.go
@@ -17,10 +17,14 @@ import (
 	vapi "github.com/hashicorp/vault/api"
 )
 
+var vaultBaseFingerprintInterval = 15 * time.Second
+
 // VaultFingerprint is used to fingerprint for Vault
 type VaultFingerprint struct {
 	logger log.Logger
 	states map[string]*vaultFingerprintState
+
+	initialized bool
 }
 
 type vaultFingerprintState struct {
@@ -77,8 +81,8 @@ func (f *VaultFingerprint) fingerprintImpl(cfg *config.VaultConfig, resp *Finger
 	status, err := state.client.Sys().SealStatus()
 	if err != nil {
 		// Print a message indicating that Vault is not available anymore
-		if state.isAvailable {
-			logger.Info("Vault is unavailable")
+		if f.initialized {
+			logger.Info("Vault is unavailable", "error", err)
 		}
 		state.isAvailable = false
 		return nil
@@ -109,6 +113,17 @@ func (f *VaultFingerprint) fingerprintImpl(cfg *config.VaultConfig, resp *Finger
 }
 
 func (f *VaultFingerprint) Periodic() (bool, time.Duration) {
+	if len(f.states) == 0 {
+		return true, vaultBaseFingerprintInterval
+	}
+	for _, state := range f.states {
+		if !state.isAvailable {
+			return true, vaultBaseFingerprintInterval
+		}
+	}
+
+	f.initialized = true
+	// Once all vaults are initially discovered and healthy we stop the fingerprint
 	return false, 0
 }
 
